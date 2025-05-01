c16c449f738dc3b50b712283cc1feed6b5b9394d
dd84ai <dd84ai@gmail.com>
2025-05-01 11:47:13 +0200

    Revert "fix: make env vars overriding defaults"
    This reverts commit 3adac28bf42e70f8b6ed408b3368a3056184fe7b.

--- typelog/envs.go
+++ typelog/envs.go
@@ -15,20 +15,6 @@ type TypelogEnvs struct {
 	EnableScopesShown bool
 }
 
-var EnvPresent struct {
-	EnableJson        bool
-	EnableFileShown   bool
-	EnableScopesShown bool
-} = struct {
-	EnableJson        bool
-	EnableFileShown   bool
-	EnableScopesShown bool
-}{
-	EnableJson:        os.Getenv(strings.ToUpper(TOOL_NAME)+"_LOG_JSON") != "" || os.Getenv(strings.ToUpper(TOOL_NAME)+"_JSON") != "",
-	EnableFileShown:   os.Getenv(strings.ToUpper(TOOL_NAME)+"_LOG_FILE_SHOWING") != "" || os.Getenv(strings.ToUpper(TOOL_NAME)+"_FILES") != "",
-	EnableScopesShown: os.Getenv(strings.ToUpper(TOOL_NAME)+"_SCOPES") != "",
-}
-
 var Env TypelogEnvs = TypelogEnvs{
 	EnableJson:        os.Getenv(strings.ToUpper(TOOL_NAME)+"_LOG_JSON") == "true" || os.Getenv(strings.ToUpper(TOOL_NAME)+"_JSON") == "true",
 	EnableFileShown:   os.Getenv(strings.ToUpper(TOOL_NAME)+"_LOG_FILE_SHOWING") == "true" || os.Getenv(strings.ToUpper(TOOL_NAME)+"_FILES") == "true",
